8114fab4a0789e80fa0ffe9cd7bdeae8b8a60713
Christine <enit.christine@gmail.com>
2025-04-18 01:49:15 +1000

    Added limit to user input

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ function createBoard(num){
         for (let j=0; j<num;j++){
             //create new square and attach it to container
             const square=document.createElement("div");
-            square.style.width="10px";
-            square.style.height ="10px";
+            let size=SIZE/num;
+            square.style.width=size+"px";
+            square.style.height =size+"px";
             square.style.backgroundColor= "black";
             square.addEventListener("mouseover",()=>{
                 square.style.backgroundColor= "blue";
@@ -29,12 +30,16 @@ const container=document.querySelector(".container");
 // container.style.justifyContent="space-around";
 
 const GRID_NUM=16;
+const SIZE=800;
 let board;
 createBoard(GRID_NUM);
 
 const button=document.querySelector("button");
 button.addEventListener("click",()=>{
-    let input = prompt("What is the size? (e.g. 16)");
+    let input = 0;
+    while (input>100 || input <1){
+        input=prompt("Please enter size between 1-100");
+    }
     container.removeChild(board);
     createBoard(input);
 })
