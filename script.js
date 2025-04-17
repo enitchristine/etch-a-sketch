14bd7b7ec4e0c6d697c0a43025e69e7021e9a6ac
Christine <enit.christine@gmail.com>
2025-04-18 01:42:09 +1000

    Added button to create new board and added createBoard function

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
 
+function createBoard(num){
+    board=document.createElement("div");
+    // board.style.display="flex";
+    for (let i=0; i<num;i++){
+        const row=document.createElement("div");
+        row.style.display="flex";
+    
+        for (let j=0; j<num;j++){
+            //create new square and attach it to container
+            const square=document.createElement("div");
+            square.style.width="10px";
+            square.style.height ="10px";
+            square.style.backgroundColor= "black";
+            square.addEventListener("mouseover",()=>{
+                square.style.backgroundColor= "blue";
+            })
+    
+            row.appendChild(square);
+        }
+        board.appendChild(row);
+    }
+    container.appendChild(board);
+}
+
+
 const container=document.querySelector(".container");
 // container.style.display="flex";
 // container.style.justifyContent="space-around";
 
 const GRID_NUM=16;
+let board;
+createBoard(GRID_NUM);
 
-for (let i=0; i<GRID_NUM;i++){
-    const row=document.createElement("div");
-    row.style.display="flex";
-
-    for (let j=0; j<GRID_NUM;j++){
-        //create new square and attach it to container
-        const square=document.createElement("div");
-        square.style.width="50px";
-        square.style.height ="50px";
-        square.style.backgroundColor= "black";
-        square.addEventListener("mouseover",()=>{
-            square.style.backgroundColor= "blue";
-        })
-
-        row.appendChild(square);
-    }
-    container.appendChild(row);
-
-}
+const button=document.querySelector("button");
+button.addEventListener("click",()=>{
+    let input = prompt("What is the size? (e.g. 16)");
+    container.removeChild(board);
+    createBoard(input);
+})
